d0fa0fdab7055fd920552465ecaf894641464d17
Muhammad Adib Ahmad Roslan <adbroslan@gmail.com>
2020-05-04 05:48:48 +0800

    added middleware for route checking

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::livewire('/home', 'home.userlist')->name('home');
+Route::livewire('/home', 'home.userlist')->name('home')->middleware('auth');
 Auth::routes();
 
 // Route::get('/home', 'HomeController@index')->name('home');
